79109a636ba29edc95e89495370e6d61593efc76
Tanya Twinkle <90620194+tanyatwinkle@users.noreply.github.com>
2023-10-15 00:19:27 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,70 @@
+import React, { useState } from 'react';
 
+const ColorPicker = () => {
+  const colorOptions = [
+    'red',
+    'blue',
+    'green',
+    'yellow',
+    'orange',
+    'purple',
+    'pink',
+    'brown',
+    'cyan',
+    'magenta',
+    'lime',
+    'teal',
+    'indigo',
+    'gray',
+    'black',
+  ];
+
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  const handleColorPick = (color) => {
+    setSelectedColor(color);
+  };
+
+  return (
+    <div
+          className="selected-color"
+          style={{
+            backgroundColor: selectedColor || 'white',
+            width: '100%',
+            height: '100%',
+            margin: '0 auto',
+            borderRadius: '25px',
+          }}
+        >
+    <div style={{ textAlign: 'center' }}>
+      <h1>Color Picker</h1>
+      <div>
+        <button onClick={() => handleColorPick(colorOptions[Math.floor(Math.random() * colorOptions.length)])}>
+          Pick a Color
+        </button>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+        {colorOptions.map((color, index) => (
+          <div
+            key={index}
+            onClick={() => handleColorPick(color)}
+            className="color-option"
+            style={{
+              backgroundColor: color,
+              width: '50px',
+              height: '50px',
+              margin: '10px',
+              cursor: 'pointer',
+              border:'5px',
+              borderRadius:'1px',
+              borderColor: 'black',
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default ColorPicker;
